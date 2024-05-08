45106eff5599d5ac4dbd6bc74ac1ee72cfefdae6
FJANNUÁRIO <felipejanuarioalves@outlook.com>
2024-05-08 01:44:29 -0300

    commit

---
+++ IrmãosMarios/scripts.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function mostrarForm(){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderForm() {
+    form.style.left = "-300px";
+    form.style.transform = "translatex(0)";
+    mascara.style.visibility = "hidden";
+}
+
+function ajustarVolume(volume) {
+    const audio = document.getElementById("myAudio");
+    audio.volume = volume;
+  }
+
+  // Ajuste de volume inicial
+  ajustarVolume(0.02); // Define o volume inicial para 20%
